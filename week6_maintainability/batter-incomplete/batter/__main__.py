746e09650c9144e311cc342421dde44eb4e023e4
beautycodez <angellorl_94@hotmail.com>
2022-12-07 20:47:25 -0500

    sdaf

--- week6_maintainability/batter-incomplete/batter/__main__.py
+++ week6_maintainability/batter-incomplete/batter/__main__.py
@@ -1,9 +1,11 @@
+import os
 from constants import *
 from game.directing.director import Director
 from game.directing.scene_manager import SceneManager
 
 
 def main():
+    os.chdir(r'batter-incomplete')
     director = Director(SceneManager.VIDEO_SERVICE)
     director.start_game()
 
